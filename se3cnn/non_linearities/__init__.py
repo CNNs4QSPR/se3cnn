cf2f24885dca9fb64784c6131c67cb264e960f31
Tess <blondegeek@gmail.com>
2019-06-06 11:53:01 -0700

    add flexible nonlinearity class for norm

--- se3cnn/non_linearities/__init__.py
+++ se3cnn/non_linearities/__init__.py
@@ -1,4 +1,5 @@
 from .norm_activation import NormRelu
 from .norm_activation import NormSoftplus
+from .norm_activation import NormActivation
 from .scalar_activation import ScalarActivation
 from .gated_activation import GatedActivation
